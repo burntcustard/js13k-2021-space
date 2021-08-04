d65de11b90503d850f48748c8db25f44b97dd930
Reece Bennett <reece.bennett1@gmail.com>
2021-08-04 23:08:23 +0100

    Change camera to store rotation as radians internally

--- src/js/camera.js
+++ src/js/camera.js
@@ -1,19 +1,28 @@
+import { toRad, toDeg } from './util';
+
 const camera = document.querySelector('.camera');
 const scene = document.querySelector('.scene');
 const cameraDebug = document.querySelector('.debug .view');
 const perfDebug = document.querySelector('.debug .perf');
 
 const defaultRotation = {
-  x: parseInt(getComputedStyle(scene).getPropertyValue('--default-rotation-x').replace('deg', ''), 10),
-  z: parseInt(getComputedStyle(scene).getPropertyValue('--default-rotation-z').replace('deg', ''), 10),
+  x: toRad(parseInt(getComputedStyle(scene).getPropertyValue('--default-rotation-x').replace('deg', ''), 10)),
+  z: toRad(parseInt(getComputedStyle(scene).getPropertyValue('--default-rotation-z').replace('deg', ''), 10)),
 };
 const rotation = { x: defaultRotation.x, z: defaultRotation.z };
 const prevRotation = { x: rotation.x, z: rotation.z };
 const position = { x: 0, y: 0, z: 0 };
 let zoom = 0;
+const rotationSpeed = 0.01;
 
 let mouseDown = false;
 const mouseDownPos = { x: 0, y: 0 };
+
+function moveCamera() {
+  camera.style.transform = `translateZ(${zoom}px)`;
+  scene.style.transform = `rotateX(${rotation.x}rad) rotateZ(${rotation.z}rad) translateX(${position.x}px) translateY(${position.y}px)`;
+  cameraDebug.innerText = `Position: ${Math.round(position.x)}x, ${Math.round(position.y)}y ${Math.round(position.z)}z\nRotation: ${Math.round(toDeg(rotation.x))}°x, ${Math.round(toDeg(rotation.z))}°z\nZoom: ${Math.round(zoom)}px`;
+}
 
 function handleDown(event) {
   event.preventDefault();
@@ -35,30 +44,28 @@ function handleMove(event) {
     event.preventDefault();
     const x = event.clientX || event.touches[0].clientX;
     const y = event.clientY || event.touches[0].clientY;
-    rotation.x = mouseDownPos.y - y + prevRotation.x;
-    rotation.z = mouseDownPos.x - x + prevRotation.z;
-    moveCamera(rotation, position);
+    rotation.x = (mouseDownPos.y - y) * rotationSpeed + prevRotation.x;
+    rotation.z = (mouseDownPos.x - x) * rotationSpeed + prevRotation.z;
+    moveCamera();
     // console.log(`Rotating ${tempRotation.x}, ${tempRotation.z}`)
   }
 }
 
-function moveCamera(rotation, position) {
-  camera.style.transform = `translateZ(${zoom}px)`;
-  scene.style.transform = `rotateX(${rotation.x}deg) rotateZ(${rotation.z}deg) translateX(${position.x}px) translateY(${position.y}px)`;
-  cameraDebug.innerText = `Position: ${Math.round(position.x)}x, ${Math.round(position.y)}y ${Math.round(position.z)}z\nRotation: ${Math.round(rotation.x)}°x, ${Math.round(rotation.z)}°z\nZoom: ${Math.round(zoom)}px`;
-}
-
+// eslint-disable-next-line no-unused-vars
 function resetRotation() {
-  console.log('Resetting rotation');
+  // console.log('Resetting rotation');
   rotation.x = defaultRotation.x;
   rotation.z = defaultRotation.z;
-  moveCamera(rotation, position);
+  moveCamera();
 }
 
+// eslint-disable-next-line no-unused-vars
 function resetPosition() {
-  console.log('Resetting position');
-  position.x = position.y = position.z = 0;
-  moveCamera(rotation, position);
+  // console.log('Resetting position');
+  position.x = 0;
+  position.y = 0;
+  position.z = 0;
+  moveCamera();
 }
 
 document.addEventListener('mousedown', handleDown);
@@ -68,7 +75,7 @@ document.addEventListener('touchend', handleUp);
 document.addEventListener('mousemove', handleMove);
 document.addEventListener('touchmove', handleMove);
 
-moveCamera(rotation, position);
+moveCamera();
 
 /* Game loop */
 
@@ -77,17 +84,13 @@ let previousTimestamp;
 const movementSpeed = 0.2;
 const zoomSpeed = 0.2;
 
-document.onkeydown = function (event) {
+document.onkeydown = (event) => {
   keys.add(event.key);
 };
 
-document.onkeyup = function (event) {
+document.onkeyup = (event) => {
   keys.delete(event.key);
 };
-
-function toRad(angle) {
-  return angle * (Math.PI / 180);
-}
 
 function main(timestamp) {
   window.requestAnimationFrame(main);
@@ -98,24 +101,21 @@ function main(timestamp) {
   // Do stuff
   let moveX = 0; let moveY = 0; let
     moveZoom = 0;
-  const rx = toRad(rotation.x);
-  const ry = toRad(rotation.y);
-  const rz = toRad(rotation.z);
   if (keys.has('w')) {
-    moveX += Math.sin(rz) * movementSpeed * elapsed;
-    moveY += Math.cos(rz) * movementSpeed * elapsed;
+    moveX += Math.sin(rotation.z) * movementSpeed * elapsed;
+    moveY += Math.cos(rotation.z) * movementSpeed * elapsed;
   }
   if (keys.has('a')) {
-    moveX += Math.cos(rz) * movementSpeed * elapsed;
-    moveY -= Math.sin(rz) * movementSpeed * elapsed;
+    moveX += Math.cos(rotation.z) * movementSpeed * elapsed;
+    moveY -= Math.sin(rotation.z) * movementSpeed * elapsed;
   }
   if (keys.has('s')) {
-    moveX -= Math.sin(rz) * movementSpeed * elapsed;
-    moveY -= Math.cos(rz) * movementSpeed * elapsed;
+    moveX -= Math.sin(rotation.z) * movementSpeed * elapsed;
+    moveY -= Math.cos(rotation.z) * movementSpeed * elapsed;
   }
   if (keys.has('d')) {
-    moveX -= Math.cos(rz) * movementSpeed * elapsed;
-    moveY += Math.sin(rz) * movementSpeed * elapsed;
+    moveX -= Math.cos(rotation.z) * movementSpeed * elapsed;
+    moveY += Math.sin(rotation.z) * movementSpeed * elapsed;
   }
   if (keys.has('z')) {
     moveZoom += zoomSpeed * elapsed;
@@ -127,7 +127,7 @@ function main(timestamp) {
     position.x += moveX;
     position.y += moveY;
     zoom += moveZoom;
-    moveCamera(rotation, position);
+    moveCamera();
   }
 
   previousTimestamp = timestamp;
---
+++ src/js/util.js
@@ -0,0 +1,7 @@
+export function toRad(angle) {
+  return angle * (Math.PI / 180);
+}
+
+export function toDeg(angle) {
+  return (angle * 180) / Math.PI;
+}
