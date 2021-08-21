341293618062913cecca9c91a768159faf4ca507
Reece Bennett <reece.bennett1@gmail.com>
2021-08-21 15:26:45 +0100

    Update pyramid faces

--- src/js/shapes/pyramid.js
+++ src/js/shapes/pyramid.js
@@ -1,47 +1,73 @@
 import Shape from './shape';
 import Face from './face';
+import { PI_2 } from '../util';
 
 export default class Pyramid extends Shape {
-  constructor(props) {
-    super({ ...props, className: 'pyramid' });
+  constructor({ w, d, h, x, y, z, rx, ry, rz }) {
+    super({
+      w,
+      d,
+      h,
+      x,
+      y,
+      z,
+      rx,
+      ry,
+      rz,
+      className: 'pyramid',
+    });
 
-    const hypotenuse = Math.hypot(this.w / 2, this.h);
-    const angle = Math.atan2(this.h * 2, this.w) * -1;
+    const wHypot = Math.hypot(w * 0.5, h);
+    const dHypot = Math.hypot(d * 0.5, h);
+    const wAngle = -Math.atan2(h * 2, w);
+    const dAngle = -Math.atan2(h * 2, d);
 
     this.sides = [
       new Face({
-        w: this.w,
-        h: hypotenuse,
-        x: 0,
-        y: this.w - hypotenuse,
-        rx: angle,
+        w,
+        h: d,
+        x: w * -0.5,
+        y: d * -1.5,
+        z: h * -0.5,
+        rx: Math.PI,
+      }),
+      new Face({
+        w,
+        h: dHypot,
+        x: w * -0.5,
+        y: d * 0.5 - dHypot,
+        z: h * -0.5,
+        rx: dAngle,
         className: 'tri',
       }),
       new Face({
-        w: this.w,
-        h: hypotenuse,
-        x: this.w / 2,
-        y: this.w / 2 - hypotenuse,
-        rx: angle,
-        rz: Math.PI / -2,
+        w: d,
+        h: wHypot,
+        x: w * 0.5 - d * 0.5,
+        y: -wHypot,
+        z: h * -0.5,
+        rx: wAngle,
+        rz: -PI_2,
         className: 'tri',
       }),
       new Face({
-        w: this.w,
-        h: hypotenuse,
-        x: 0,
-        y: -hypotenuse,
-        rx: angle,
+        w,
+        h: dHypot,
+        x: w * -0.5,
+        y: d * -0.5 - dHypot,
+        z: h * -0.5,
+        rx: dAngle,
         rz: Math.PI,
         className: 'tri',
       }),
       new Face({
-        w: this.w,
-        h: hypotenuse,
-        x: this.w / -2,
-        y: this.w / 2 - hypotenuse,
-        rx: angle,
-        rz: Math.PI / 2,
+        w: d,
+        h: wHypot,
+        x: w * -0.5 - d * 0.5,
+        y: -wHypot,
+        z: h * -0.5,
+        rx: wAngle,
+        rz: PI_2,
         className: 'tri',
       }),
     ];
