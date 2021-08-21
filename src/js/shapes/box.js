84d476885d07bbb28ee8f382359ba4dcd4351b1d
Reece Bennett <reece.bennett1@gmail.com>
2021-08-21 15:32:37 +0100

    Fix box faces

--- src/js/shapes/box.js
+++ src/js/shapes/box.js
@@ -4,70 +4,80 @@ import { PI_2 } from '../util';
 import Vec3 from '../vec3';
 
 export default class Box extends Shape {
-  constructor(props) {
-    super({ ...props, className: 'box' });
+  constructor({ w, d, h, x, y, z, rx, ry, rz }) {
+    super({
+      w,
+      d,
+      h,
+      x,
+      y,
+      z,
+      rx,
+      ry,
+      rz,
+      className: 'box',
+    });
 
     this.sides = [
       new Face({
-        w: this.w,
-        h: this.d,
-        x: this.w * -0.5,
-        y: this.d * -1.5,
-        z: this.h * -0.5,
+        w,
+        h: d,
+        x: w * -0.5,
+        y: d * -1.5,
+        z: h * -0.5,
         rx: Math.PI,
         colour: new Vec3(1, 1, 1),
       }),
       new Face({
-        w: this.w,
-        h: this.h,
-        x: this.w * -0.5,
-        y: this.d * 0.5 - this.h,
-        z: this.h * -0.5,
+        w,
+        h,
+        x: w * -0.5,
+        y: d * 0.5 - h,
+        z: h * -0.5,
         rx: -PI_2,
         colour: new Vec3(1, 1, 1),
       }),
       new Face({
-        w: this.d,
-        h: this.h,
-        y: -this.h,
-        z: this.h * -0.5,
+        w: d,
+        h,
+        x: w * 0.5 - d * 0.5,
+        y: -h,
+        z: h * -0.5,
         rx: -PI_2,
         rz: -PI_2,
         colour: new Vec3(1, 1, 1),
       }),
       new Face({
-        w: this.w,
-        h: this.h,
-        x: this.w * -0.5,
-        y: this.d * -0.5 - this.h,
-        z: this.h * -0.5,
+        w,
+        h,
+        x: w * -0.5,
+        y: d * -0.5 - h,
+        z: h * -0.5,
         rx: -PI_2,
         rz: Math.PI,
         colour: new Vec3(1, 1, 1),
       }),
       new Face({
-        w: this.d,
-        h: this.h,
-        x: -this.w,
-        y: -this.h,
-        z: this.h * -0.5,
+        w: d,
+        h,
+        x: w * -0.5 - d * 0.5,
+        y: -h,
+        z: h * -0.5,
         rx: -PI_2,
         rz: PI_2,
         colour: new Vec3(1, 1, 1),
       }),
       new Face({
-        w: this.w,
-        h: this.d,
-        x: this.w * -0.5,
-        y: this.d * -0.5,
-        z: this.h * 0.5,
+        w,
+        h: d,
+        x: w * -0.5,
+        y: d * -0.5,
+        z: h * 0.5,
         colour: new Vec3(1, 1, 1),
       }),
     ];
 
-    // this.element.append(this.sides.map((s) => s.element));
     this.element.append(...this.sides.map((side) => side.element));
-
     super.update();
   }
 }
