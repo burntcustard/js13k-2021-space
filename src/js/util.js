59c8ae24a28a72219f314085d6cd66fa8e0cdd7a
burntcustard <burntcustard@gmail.com>
2021-08-27 00:03:12 +0100

    Disable prefer-destructuring for Math.PI var

--- src/js/util.js
+++ src/js/util.js
@@ -4,6 +4,7 @@ export const toDeg = (angle) => (angle * 180) / Math.PI;
 
 export const $ = (selectors) => document.querySelector(selectors);
 
+/* eslint-disable-next-line prefer-destructuring */
 export const PI = Math.PI;
 export const PI_2 = Math.PI / 2;
 export const PI_3 = Math.PI / 3;
