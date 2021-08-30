4f1bbe94b411fcd839dccc9e9edcfe30e64085ed
Reece Bennett <reece.bennett1@gmail.com>
2021-08-30 17:22:46 +0100

    Change box faces to be centred

--- src/js/shapes/box.js
+++ src/js/shapes/box.js
@@ -1,74 +1,66 @@
 import Shape from './shape';
 import Face from './face';
 import { PI_2 } from '../util';
-import Vec3 from '../vec3';
 
 /**
  * Create a box.
- * Depth default to width if ommitted.
+ * Depth defaults to width if ommitted.
  * @param {*} properties
  */
-export default function Box({
-  w, d = w, h, x, y, z, rx, ry, rz, className,
-}) {
+export default function Box({ w, d = w, h, x, y, z, rx, ry, rz, className }) {
   this.className = `${className ?? ''} box`;
   Shape.call(this, { w, d, h, x, y, z, rx, ry, rz, className: this.className });
+
+  const W_2 = w * 0.5;
+  const D_2 = d * 0.5;
+  const H_2 = h * 0.5;
+
   this.sides = [
     new Face({
       w,
       h: d,
-      x: w * -0.5,
-      y: d * -1.5,
-      z: h * -0.5,
+      x: -W_2,
+      y: -D_2,
+      z: -H_2,
       rx: Math.PI,
-      colour: new Vec3(1, 1, 1),
     }),
     new Face({
       w,
       h,
-      x: w * -0.5,
-      y: d * 0.5 - h,
-      z: h * -0.5,
+      x: -W_2,
+      y: D_2 - H_2,
       rx: -PI_2,
-      colour: new Vec3(1, 1, 1),
     }),
     new Face({
       w: d,
       h,
-      x: w * 0.5 - d * 0.5,
-      y: -h,
-      z: h * -0.5,
+      x: W_2 - D_2,
+      y: -H_2,
       rx: -PI_2,
       rz: -PI_2,
-      colour: new Vec3(1, 1, 1),
     }),
     new Face({
       w,
       h,
-      x: w * -0.5,
-      y: d * -0.5 - h,
-      z: h * -0.5,
+      x: -W_2,
+      y: -D_2 - H_2,
       rx: -PI_2,
       rz: Math.PI,
-      colour: new Vec3(1, 1, 1),
     }),
     new Face({
       w: d,
       h,
-      x: w * -0.5 - d * 0.5,
-      y: -h,
-      z: h * -0.5,
+      x: -W_2 - D_2,
+      y: -H_2,
       rx: -PI_2,
       rz: PI_2,
-      colour: new Vec3(1, 1, 1),
     }),
     new Face({
       w,
       h: d,
-      x: w * -0.5,
-      y: d * -0.5,
-      z: h * 0.5,
-      colour: new Vec3(1, 1, 1),
+      x: -W_2,
+      y: -D_2,
+      z: H_2,
     }),
   ];
   this.element.append(...this.sides.map((side) => side.element));
