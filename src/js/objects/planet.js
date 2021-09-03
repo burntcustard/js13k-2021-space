5f9645c2806eca0fe58727c35d32510e23b3d601
burntcustard <burntcustard@gmail.com>
2021-09-03 11:27:33 +0100

    --wip-- Trying to fix planet hoola hooping

--- src/js/objects/planet.js
+++ src/js/objects/planet.js
@@ -5,34 +5,47 @@ export default function Planet({ x, y, z, r }) {
   this.y = y ?? 0;
   this.z = z ?? 0;
   this.r = r ?? 0;
+  this.ring = {};
   this.element = document.createElement('div');
-  this.element.className = 'face circle planet';
-  this.element.style.background = '#139';
-  this.element.style.width = `${r * 2}px`;
-  this.element.style.height = `${r * 2}px`;
+  this.element.body = document.createElement('div');
+  this.element.ring = document.createElement('div');
+  this.element.ring.style.width = `${r * 16}px`;
+  this.element.ring.style.height = `${r * 16}px`;
+  this.element.className = 'planet-wrap';
+  this.element.body.className = 'face circle planet';
+  this.element.ring.className = 'ring';
+  this.element.append(this.element.body, this.element.ring);
+  this.element.body.style.background = '#139';
+  this.element.body.style.width = `${r * 16}px`;
+  this.element.body.style.height = `${r * 16}px`;
   // TODO: A new filter for whatever type of planet this is
   // this.element.style.filter = 'url(#noise)';
+  this.element.ring.style.transform = `
+    scale(2)
+  `;
   document.querySelector('.scene').append(this.element);
   camera.followers.push(this);
   this.updateTransform = () => {
-    const clientRect = this.element.getBoundingClientRect();
-    const xOffset = clientRect.left + clientRect.right - window.innerWidth;
-    const yOffset = clientRect.top + clientRect.bottom - window.innerWidth;
-
-    // TODO: Include camera distance in the translate so that far away
-    // planets get x/y offsetted MORE so that they don't eat nearby objects(?)
-    const xDist = Math.abs(this.x - camera.x);
-    const yDist = Math.abs(this.y - camera.y);
+    // const clientRect = this.element.getBoundingClientRect();
+    // const xOffset = clientRect.left + clientRect.right - window.innerWidth;
+    // const yOffset = clientRect.top + clientRect.bottom - window.innerWidth;
+    //
+    // // TODO: Include camera distance in the translate so that far away
+    // // planets get x/y offsetted MORE so that they don't eat nearby objects(?)
+    // const xDist = Math.abs(this.x - camera.x);
+    // const yDist = Math.abs(this.y - camera.y);
 
     this.element.style.transform = `
       translate3D(${this.x}px, ${this.y}px, ${this.z}px)
+    `;
+    this.element.body.style.transform = `
       rotateZ(${-camera.rz}rad)
       rotateY(${-camera.ry}rad)
       rotateX(${-camera.rx}rad)
-      translate(${xOffset}px, ${yOffset}px)
-      scale(32)
+      scale(2)
       rotate(90deg)
     `;
+    // translate(${xOffset}px, ${yOffset}px)
   };
   this.updateTransform();
 }
--- src/js/settings.js
+++ src/js/settings.js
@@ -4,7 +4,7 @@ const settings = {
     perspective: 500,
     rotateSpeed: 0.01,
     zoom: {
-      max: 4000,
+      max: 40000,
       default: 500,
       min: 100,
     },
