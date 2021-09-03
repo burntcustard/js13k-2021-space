102576fff3b1b289ef797c021cb732c0c43b57fc
burntcustard <burntcustard@gmail.com>
2021-09-03 15:00:00 +0100

    --wip-- cleaned up planet rotation stuff a little

--- src/js/objects/planet.js
+++ src/js/objects/planet.js
@@ -7,45 +7,53 @@ export default function Planet({ x, y, z, r }) {
   this.r = r ?? 0;
   this.ring = {};
   this.element = document.createElement('div');
+  this.element.className = 'planet';
   this.element.body = document.createElement('div');
-  this.element.ring = document.createElement('div');
-  this.element.ring.style.width = `${r * 16}px`;
-  this.element.ring.style.height = `${r * 16}px`;
-  this.element.className = 'planet-wrap';
-  this.element.body.className = 'face circle planet';
-  this.element.ring.className = 'ring';
-  this.element.append(this.element.body, this.element.ring);
   this.element.body.style.background = '#139';
   this.element.body.style.width = `${r * 16}px`;
   this.element.body.style.height = `${r * 16}px`;
+  this.element.body.className = 'face circle body';
+  this.element.ring = document.createElement('div');
+  this.element.ring.className = 'ring';
+  this.element.ring.style.width = `${r * 16}px`;
+  this.element.ring.style.height = `${r * 16}px`;
+  this.element.append(this.element.body, this.element.ring);
   // TODO: A new filter for whatever type of planet this is
   // this.element.style.filter = 'url(#noise)';
   this.element.ring.style.transform = `
     scale(2)
   `;
   document.querySelector('.scene').append(this.element);
+
+  // When the camera moves, this will updateTransform();
   camera.followers.push(this);
+
   this.updateTransform = () => {
+    // This was an attempt to take into account the position of the circle
+    // on the screen to adjust it slightly to stop the hula hooping, but is meh
     // const clientRect = this.element.getBoundingClientRect();
     // const xOffset = clientRect.left + clientRect.right - window.innerWidth;
     // const yOffset = clientRect.top + clientRect.bottom - window.innerWidth;
-    //
-    // // TODO: Include camera distance in the translate so that far away
-    // // planets get x/y offsetted MORE so that they don't eat nearby objects(?)
     // const xDist = Math.abs(this.x - camera.x);
     // const yDist = Math.abs(this.y - camera.y);
+    // translate(${xOffset}px, ${yOffset}px)
 
+    // Position the celestial body - this shouldn't change much so maybe don't do
+    // every update?
     this.element.style.transform = `
       translate3D(${this.x}px, ${this.y}px, ${this.z}px)
     `;
+
+    // Make the circle face the camera so it looks like a sphere.
+    // the rotate90 is just temporary to make the lighting line up
     this.element.body.style.transform = `
       rotateZ(${-camera.rz}rad)
-      rotateY(${-camera.ry}rad)
       rotateX(${-camera.rx}rad)
       scale(2)
       rotate(90deg)
     `;
-    // translate(${xOffset}px, ${yOffset}px)
+    // rotateY(${-camera.ry}rad) // We don't actually need Y rotation?
   };
+
   this.updateTransform();
 }
