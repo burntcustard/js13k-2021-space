cdc76a0c80dc51bde1c7ff44425f095a761f14de
burntcustard <burntcustard@gmail.com>
2021-09-03 15:30:53 +0100

    Minor cleanup of unused things, but FF performance is baaaaad

--- src/js/main.js
+++ src/js/main.js
@@ -26,7 +26,7 @@ const skybox = new Cubemap({
 
 const sun = new Sun({ x: 40000, y: 40000, z: 10000, r: 200 });
 
-const planet = new Planet({ x: 800, y: -8000, r: 300 });
+const planet = new Planet({ x: 900, y: -9000, r: 300 });
 
 const box = new Box({
   w: 60,
@@ -65,27 +65,27 @@ octagon.spawn();
 gameObjectList.push(box, octagon, pyramid, stationBlock, stationSolar);
 
 // TODO: Put this somewhere else
-const canvas = document.getElementsByTagName('canvas')[0];
-const feImageSphereDistort = document.getElementById('fe-image-sphere-distort');
-const ctx = canvas.getContext('2d');
-
-for (let y = 0; y < canvas.height; y++) {
-  for (let x = 0; x < canvas.width; x++) {
-    const dx = x - 32;
-    const dy = y - 32;
-    const l = Math.sqrt(dx * dx + dy * dy);
-    const a = l < 32 ? Math.asin(l / 32) : 0;
-    const z = l < 32 ? 64 - Math.cos(a) * 64 : 0;
-    const r = l < 32 ? 32 + (dx / 32) * (z / 64) * 32 : 0;
-    const g = l < 32 ? 32 + (dy / 32) * (z / 64) * 32 : 0;
-    const o = l > 30 ? Math.max(0, 1 - (l - 31) / 4) : 1;
-
-    ctx.fillStyle = `rgba(${Math.floor(r) * 4},${Math.floor(g) * 4},0,${o})`;
-    ctx.fillRect(x, y, 1, 1);
-  }
-}
-
-feImageSphereDistort.setAttribute('xlink:href', canvas.toDataURL());
+// const canvas = document.getElementsByTagName('canvas')[0];
+// const feImageSphereDistort = document.getElementById('fe-image-sphere-distort');
+// const ctx = canvas.getContext('2d');
+//
+// for (let y = 0; y < canvas.height; y++) {
+//   for (let x = 0; x < canvas.width; x++) {
+//     const dx = x - 32;
+//     const dy = y - 32;
+//     const l = Math.sqrt(dx * dx + dy * dy);
+//     const a = l < 32 ? Math.asin(l / 32) : 0;
+//     const z = l < 32 ? 64 - Math.cos(a) * 64 : 0;
+//     const r = l < 32 ? 32 + (dx / 32) * (z / 64) * 32 : 0;
+//     const g = l < 32 ? 32 + (dy / 32) * (z / 64) * 32 : 0;
+//     const o = l > 30 ? Math.max(0, 1 - (l - 31) / 4) : 1;
+//
+//     ctx.fillStyle = `rgba(${Math.floor(r) * 4},${Math.floor(g) * 4},0,${o})`;
+//     ctx.fillRect(x, y, 1, 1);
+//   }
+// }
+//
+// feImageSphereDistort.setAttribute('xlink:href', canvas.toDataURL());
 
 const lights = [
   new Light({
--- src/js/objects/planet.js
+++ src/js/objects/planet.js
@@ -8,20 +8,23 @@ export default function Planet({ x, y, z, r }) {
   this.ring = {};
   this.element = document.createElement('div');
   this.element.className = 'planet';
+  this.element.style.transform = `
+  translate3D(${this.x}px, ${this.y}px, ${this.z}px)
+  `;
   this.element.body = document.createElement('div');
   this.element.body.style.background = '#139';
-  this.element.body.style.width = `${r * 16}px`;
-  this.element.body.style.height = `${r * 16}px`;
+  this.element.body.style.width = `${r * 2}px`;
+  this.element.body.style.height = `${r * 2}px`;
   this.element.body.className = 'face circle body';
   this.element.ring = document.createElement('div');
   this.element.ring.className = 'ring';
-  this.element.ring.style.width = `${r * 16}px`;
-  this.element.ring.style.height = `${r * 16}px`;
+  this.element.ring.style.width = `${r * 8}px`;
+  this.element.ring.style.height = `${r * 8}px`;
   this.element.append(this.element.body, this.element.ring);
   // TODO: A new filter for whatever type of planet this is
   // this.element.style.filter = 'url(#noise)';
   this.element.ring.style.transform = `
-    scale(2)
+    scale3D(8, 8, 8)
   `;
   document.querySelector('.scene').append(this.element);
 
@@ -40,16 +43,13 @@ export default function Planet({ x, y, z, r }) {
 
     // Position the celestial body - this shouldn't change much so maybe don't do
     // every update?
-    this.element.style.transform = `
-      translate3D(${this.x}px, ${this.y}px, ${this.z}px)
-    `;
 
     // Make the circle face the camera so it looks like a sphere.
     // the rotate90 is just temporary to make the lighting line up
     this.element.body.style.transform = `
       rotateZ(${-camera.rz}rad)
       rotateX(${-camera.rx}rad)
-      scale(2)
+      scale(32)
       rotate(90deg)
     `;
     // rotateY(${-camera.ry}rad) // We don't actually need Y rotation?
