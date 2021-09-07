778663f6ade6307f3d519940180de5253898f1aa
burntcustard <burntcustard@gmail.com>
2021-09-07 02:40:06 +0100

    --wip-- stretch solar panels

--- src/js/game-object-list.js
+++ src/js/game-object-list.js
@@ -18,4 +18,12 @@ gameObjectList.killSelected = function () {
   $('.ui-panel--btns').setAttribute('aria-hidden', true);
 };
 
+gameObjectList.upgradeSelected = function () {
+  for (let i = this.length - 1; i > 0; i--) {
+    if (this[i].selected && this[i].upgrade) {
+      this[i].upgrade();
+    }
+  }
+};
+
 export default gameObjectList;
--- src/js/modules/solar.js
+++ src/js/modules/solar.js
@@ -1,5 +1,6 @@
 import Module from './module';
 import Box from '../shapes/box';
+import Vec3 from '../vec3';
 
 const info = {
   tag: 'Solar Panel Basic',
@@ -29,6 +30,7 @@ export default function Solar({
   });
   this.model.sides[1].element.className += ' panel';
   this.model.sides[4].element.className += ' panel';
+  this.level = 1;
 
   Module.call(this, { x, y, z, rx, ry, rz, ...info });
 }
@@ -37,7 +39,35 @@ Object.assign(Solar, info);
 Solar.prototype = Object.create(Module.prototype);
 Solar.prototype.constructor = Solar;
 
+Solar.prototype.instancesBuilt = 0;
+
+Solar.prototype.upgrade = function () {
+  this.level++;
+
+  this.model.changeSize({ w: info.w * this.level });
+
+  const shape = this.connectedTo.parent;
+
+  // Side rotated with shape's rotation
+  const sideRotated = new Vec3(this.connectedTo.x, this.connectedTo.y, this.connectedTo.z)
+    .rotateX(shape.rx)
+    .rotateY(shape.ry)
+    .rotateZ(shape.rz);
+
+  // const sideNormalised = sideRotated.normalise();
+
+  // Half model width in direction of side
+  const sideResized = sideRotated.resize(this.model.w * 0.5);
+
+  this.model.x = shape.x + sideRotated.x + sideResized.x;
+  this.model.y = shape.y + sideRotated.y + sideResized.y;
+  this.model.z = shape.z + sideRotated.z + sideResized.z;
+  // this.model.rx = Build.rotation;
+  // this.model.ry = -Math.asin(sideNormalised.z);
+  // this.model.rz = Math.atan2(sideRotated.y, sideRotated.x);
+};
+
 Solar.prototype.build = function () {
-  // TODO: Solar panel build animation
   Module.prototype.build.call(this);
+  Solar.prototype.instancesBuilt++;
 };
