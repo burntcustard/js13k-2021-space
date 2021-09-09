1d5302b5301b2454f0fb50c60f13d7aa58999629
burntcustard <burntcustard@gmail.com>
2021-09-09 22:27:51 +0100

    Actually add new scaffolding block files

---
+++ src/js/modules/scaffold.js
@@ -0,0 +1,49 @@
+import Module from './module';
+import BoxScaffold from '../shapes/box-scaffold';
+import Build from '../build';
+
+const info = {
+  tag: 'Scaffold', // Can't use 'name' because is reserved
+  desc: 'Cheap alternative to the block',
+  className: 'scaffold module',
+  cost: 1,
+  power: 0,
+  w: 60,
+  d: 60,
+  h: 60,
+};
+
+export default function Scaffold({
+  x, y, z, rx, ry, rz,
+}) {
+  this.model = new BoxScaffold({
+    w: info.w,
+    h: info.h,
+    d: info.d,
+    x,
+    y,
+    z,
+    rx,
+    ry,
+    rz,
+    className: info.className,
+  });
+
+  Module.call(this, { x, y, z, rx, ry, rz, ...info });
+
+  // Add buildableness to all the sides of the cube
+  this.model.sides.forEach((side) => {
+    // TODO: Don't add buildableness to the side touching the existing shape side
+    Build.addEventListenersTo(side);
+  });
+  // TODO: Add hover selecty (not building) even listeners? Or just use :hover?
+}
+
+Object.assign(Scaffold, info);
+Scaffold.prototype = Object.create(Module.prototype);
+Scaffold.prototype.constructor = Scaffold;
+
+Scaffold.prototype.build = function () {
+  // TODO: Block build animation
+  Module.prototype.build.call(this);
+};
---
+++ src/js/shapes/box-scaffold.js
@@ -0,0 +1,105 @@
+import Shape from './shape';
+import Face from './face';
+import { PI, PI_2 } from '../util';
+
+/**
+ * Create a box.
+ * Depth defaults to width if ommitted.
+ * @param {*} properties
+ */
+export default function Box({ w, d = w, h, x, y, z, rx, ry, rz, className }) {
+  this.className = `${className ?? ''} box`;
+  Shape.call(this, { w, d, h, x, y, z, rx, ry, rz, className: this.className });
+
+  const W_2 = w * 0.5;
+  const D_2 = d * 0.5;
+  const H_2 = h * 0.5;
+
+  this.sides = [
+    // Outside
+    new Face({
+      w: d,
+      h,
+      x: -W_2,
+      rx: -PI_2,
+      rz: PI_2,
+    }),
+    new Face({
+      w,
+      h: d,
+      z: -H_2,
+      rx: PI,
+    }),
+    new Face({
+      w,
+      h,
+      y: D_2,
+      rx: -PI_2,
+    }),
+    new Face({
+      w,
+      h,
+      y: -D_2,
+      rx: PI_2,
+    }),
+    new Face({
+      w,
+      h: d,
+      z: H_2,
+    }),
+    new Face({
+      w: d,
+      h,
+      x: W_2,
+      rx: -PI_2,
+      rz: -PI_2,
+    }),
+    // Inside
+    new Face({
+      w: d,
+      h,
+      x: W_2,
+      rx: -PI_2,
+      rz: PI_2,
+    }),
+    new Face({
+      w,
+      h: d,
+      z: H_2,
+      rx: PI,
+    }),
+    new Face({
+      w,
+      h,
+      y: -D_2,
+      rx: -PI_2,
+    }),
+    new Face({
+      w,
+      h,
+      y: D_2,
+      rx: PI_2,
+    }),
+    new Face({
+      w,
+      h: d,
+      z: -H_2,
+    }),
+    new Face({
+      w: d,
+      h,
+      x: -W_2,
+      rx: -PI_2,
+      rz: -PI_2,
+    }),
+  ];
+
+  this.sides.forEach((side) => {
+    side.parent = this;
+  });
+
+  this.element.append(...this.sides.map((side) => side.element));
+}
+
+Box.prototype = Object.create(Shape.prototype);
+Box.prototype.constructor = Box;
