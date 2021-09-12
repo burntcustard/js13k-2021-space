1d3a261ee814a9b596adfe9b6d6e052879cb5cec
Reece Bennett <reece.bennett1@gmail.com>
2021-09-12 14:52:14 +0100

    Add resources to save file

--- src/js/save-load.js
+++ src/js/save-load.js
@@ -1,32 +1,38 @@
 import gameObjectList from './game-object-list';
 import moduleList from './modules/module-list';
+import resources from './resources';
 import { $ } from './util';
 
 const { localStorage } = window;
 
 function stringify() {
-  return JSON.stringify(
-    gameObjectList.map(({ tag, x, y, z, rx, ry, rz }) => ({ tag, x, y, z, rx, ry, rz })),
-  );
-}
-
-function parse(data) {
-  return JSON.parse(data).map(({ tag, x, y, z, rx, ry, rz }) => {
-    const module = moduleList.get(tag);
-    // eslint-disable-next-line new-cap
-    return new module({ x, y, z, rx, ry, rz });
+  return JSON.stringify({
+    objects: gameObjectList.map(({ tag, x, y, z, rx, ry, rz }) => ({ tag, x, y, z, rx, ry, rz })),
+    resources,
   });
 }
 
 function load(data) {
+  const parsed = JSON.parse(data);
+
+  // Delete all game objects then replace with save data
   for (let i = gameObjectList.length - 1; i >= 0; i--) {
     gameObjectList[i].kill();
   }
-  gameObjectList.splice(0, gameObjectList.length, ...parse(data));
+  const savedObjects = parsed.objects.map((saved) => {
+    const Module = moduleList.get(saved.tag);
+    return new Module(saved);
+  });
+  gameObjectList.splice(0, gameObjectList.length, ...savedObjects);
   gameObjectList.forEach((object) => {
     object.spawn();
     object.enable();
   });
+
+  // Load resources
+  resources.mats = parsed.resources.mats;
+  resources.power = parsed.resources.power;
+  resources.population = parsed.resources.population;
 }
 
 export default function initSaveAndLoad() {
