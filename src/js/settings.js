81723e4769c8aa306e7da55a799b135a28dd67dd
burntcustard <burntcustard@gmail.com>
2021-09-12 23:05:40 +0100

    Sun & planet tweaks

--- src/js/settings.js
+++ src/js/settings.js
@@ -4,9 +4,9 @@ const settings = {
     perspective: 500,
     rotateSpeed: 0.01,
     zoom: {
-      max: 40000,
+      max: 10000,
       default: 500,
-      min: 100,
+      min: 300,
     },
     zoomSpeed: 1,
   },
